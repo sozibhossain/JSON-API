64ef08c42d45a137564f415672370250f1d70619
sozibhossain <sozibhossain69@gmail.com>
2024-02-29 12:55:33 +0600

    inner text

--- api.js
+++ api.js
@@ -8,8 +8,31 @@
 //     .then((json) => console.log(json.title));
 // }
 
+// function loadData() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((data) => displayUser(data));
+// }
+
+// function displayUser(data) {
+//   for(const user of data){
+//     console.log(user)
+//   }
+// }
+
+
 function loadData() {
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
-    .then((json) => console.log(json.length));
+    .then((data) => displayUser(data));
+}
+
+function displayUser(data) {
+    const ul = document.getElementById('users')
+  for(const user of data){
+    console.log(user.name)
+    const li = document.createElement('li');
+    li.innerText = user.name;
+    ul.appendChild(li);
+  }
 }
